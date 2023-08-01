adf338357f41a28d1e2fd09f22f4123ecf13a82f
e-labInnovations <mohammedashad23@gmail.com>
2023-08-01 08:57:22 +0530

    linked list started

---
+++ linked-lists/01-intro.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int value;
+    struct Node* next;
+} Node;
+
+int main() {
+    Node root, elem2;
+
+    root.value = 5;
+    root.next = malloc(sizeof(Node));
+
+    root.next->value = 10;
+    root.next->next = NULL;
+
+    printf("First: %d\n", root.value);
+    printf("Second: %d\n", root.next->value);
+
+    return 0;
+}
